1992c6113462d8336f79395039f5e27dd40f511f
Khimm319 <89382986+Khimm319@users.noreply.github.com>
2022-03-07 21:07:40 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!-- Bootstrap CSS -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+    <title>Nakhon Si Thammarat Project</title>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Sarabun"
+    />
+    <style>
+      body {
+        font-family: "Sarabun";
+        background-color: #fdf2e7;
+      }
+      #container {
+        box-shadow: 6px 4px 8px 6px rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+      }
+      #header {
+        height: 160px;
+        background-image: url("P18.png");
+        background-size:100%;
+      }
+      #sidebar {
+        background-color: #f1f1f1;
+        padding: 0px;
+      }
+      #content {
+        padding: 10px;
+        background-color: #F5F5F5;
+      }
+      #footer {
+        height: 180px;
+        padding: 50px;
+        background-color: #DCDCDC;
+        border-radius: 0 0 15px 15px;
+      }
+      ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        background-color: #f1f1f1;
+      }
+      li a {
+        display: block;
+        color: #000;
+        padding: 8px 16px;
+        text-decoration: none;
+      }
+      /* Change the link color on hover */
+      li a:hover {
+        background-color: #555;
+        color:#FFFFFF;
+      }
+      p{
+        margin-top: 10px;
+      }
+    </style>
+  </head>
+  <body style="background-color:#ffd3b2">
+    <!-- คอนเทนเนอร์ -->
+    <div class="container">
+      <div id="container">
+      <div class="row">
+        <div class="col-12" id="header">
+          <!-- เฮดเดอร์ -->
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-2" id="sidebar">
+          <!-- ไซด์บาร์ -->
+          <ul>
+            <li><a href="https://number6-11.herokuapp.com/">หน้าแรก</a></li>
+            <li><a href="https://number6-11.herokuapp.com/page=1.php">แหล่งท่องเที่ยว</a></li> 
+            <li><a href="https://number6-11.herokuapp.com/page=2.php">ประเพณีและวัฒนธรรม</a></li> 
+            <li><a href="https://number6-11.herokuapp.com/page=3.php">อาหารประจำท้องถิ่น</a></li> 
+            <li><a href="https://number6-11.herokuapp.com/page=4.php">ผู้จัดทำ</a></li>
+         </ul>
+        </div>
+        <div class="col-10" id="content">
+          <!-- คอนเท้นต์ -->
+          <h4>
+           ขอนแก่น
+         </h4>
+          <center><img src="P1.png" class="img-fluid rounded"></center>
+          <br>
+         <h5>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ขอนแก่น เป็นจังหวัดที่มีขนาดพื้นที่ใหญ่เป็นอันดับที่ 6 ของภาคตะวันออกเฉียงเหนือ และมีประชากรมากเป็นอันดับที่ 3 ของภาคตะวันออกเฉียงเหนือ อยู่ในกลุ่มจังหวัดภาคตะวันออกเฉียงเหนือตอนกลาง คือ จังหวัดร้อยเอ็ด ขอนแก่น มหาสารคาม กาฬสินธุ์ โดยขอนแก่นเป็นจังหวัดที่เป็นศูนย์ปฏิบัติการของกลุ่ม
+            </h5>
+           <br></br>
+           <h5>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;จังหวัดขอนแก่นมีเทศบาลนครขอนแก่นเป็นศูนย์กลางของจังหวัด ซึ่งตั้งอยู่ในจุดที่ถนนมิตรภาพ (ทางหลวงแผ่นดินหมายเลข 2) และทางหลวงแผ่นดินหมายเลข 12 (ถนนสายเศรษฐกิจตะวันออก-ตะวันตก) ตัดผ่าน ซึ่งเป็นเส้นทางสำคัญอีกเส้นหนึ่งในการเดินทางจากภาคตะวันออกเฉียงเหนือตอนกลางเข้าไปสู่ภาคเหนือตอนล่างที่อำเภอหล่มสัก จังหวัดเพชรบูรณ์ และเดินทางเข้าสู่ประเทศลาวทางด้านทิศใต้ของลาว อาณาเขตทางทิศเหนือติดกับจังหวัดเลย จังหวัดหนองบัวลำภู และจังหวัดอุดรธานี ทิศตะวันออกติดกับจังหวัดมหาสารคามและจังหวัดกาฬสินธุ์ ทิศใต้ติดกับจังหวัดบุรีรัมย์และจังหวัดนครราชสีมา ทิศตะวันตกติดกับจังหวัดชัยภูมิและจังหวัดเพชรบูรณ์
+            </h5>
+           <br></br>
+          <h4>
+           การปกครองส่วนท้องถิ่น
+         </h4>
+         <br>
+          <h5>
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;มีจำนวนทั้งสิ้น 225 แห่ง แบ่งออกเป็น องค์การบริหารส่วนจังหวัด 1 แห่ง เทศบาลนคร 1 แห่ง เทศบาลเมือง 6 แห่ง เทศบาลตำบล 77 แห่ง และ องค์การบริหารส่วนตำบล 140 แห่ง
+          </h5>
+          <br>
+          <h5 align="center">แผนที่จังหวัด</h5>
+          <br><br>
+         <center><img src="map.png" class="img-fluid rounded"></center>
+        <br><br>
+          <h6 align="center">
+            การปกครองส่วนภูมิภาค
+          </h6>
+          <table align="center">
+            <tbody>
+              <tr valign ="top">
+          <td>
+            <ol>
+              <li>
+               อำเภอเมืองขอนแก่น
+                </li>
+              <li>
+               อำเภอบ้านฝาง
+                </li>
+              <li>
+               อำเภอพระยืน
+              </li>
+              <li>
+                อำเภอหนองเรือ
+                </li>
+              <li>    
+                อำเภอชุมแพ
+                </li>
+                <li>
+                อำเภอสีชมพู
+                    </li>
+              <li>  
+                อำเภอน้ำพอง
+                  </li>
+              <li>         
+               อำเภออุบลรัตน์          
+                  </li>
+              <li>       
+                อำเภอกระนวน         
+                  </li>
+              <li>  
+                อำเภอบ้านไผ่         
+                  </li>
+              <li>             
+               อำเภอเปือยน้อย
+           
+                  </li>
+              <li>     
+               อำเภอพล
+                  </li>
+                  <li>     
+               อำเภอแวงใหญ่
+                  </li>
+            </ol>
+      </td>
+      <td>
+            <ol start="14">
+                   <li>      
+                อำเภอแวงน้อย          
+                </li>
+              <li>         
+                อำเภอหนองสองห้อง       
+                </li>
+              <li>
+                อำเภอภูเวียง
+              </li>
+              <li>
+                อำเภอมัญจาคีรี  
+                </li>
+              <li>    
+                อำเภอชนบท         
+                </li>
+                <li>
+                อำเภอเขาสวนกวาง       
+                    </li>
+              <li>           
+                อำเภอภูผาม่าน     
+                  </li>
+              <li>  
+                 อำเภอซำสูง    
+                  </li>
+              <li>  
+                อำเภอโคกโพธิ์ไชย
+                  </li>
+              <li>
+                อำเภอหนองนาคำ    
+                  </li>
+              <li>                
+                อำเภอบ้านแฮด
+                  </li>
+                  <li>                
+                อำเภอโนนศิลา
+                  </li>
+                  <li>                
+                อำเภอเวียงเก่า
+                  </li>
+            </ol>
+      </td>
+              </tr>
+            </tbody>
+      </table>
+           <br>
+          <h5>
+            ประชากร
+          </h5>
+          <br>
+          <h5>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;จังหวัดขอนแก่นมีประชากรทั้งสิ้น 1,790,055 คน นับเป็น 576,964 ครัวเรือน นับเป็นประชากรที่อาศัยอยู่ในเขตอำเภอเมือง 1,802,872 คน (ณ ปี พ.ศ. 2563)
+          </h5>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12" id="footer">
+          <!-- ฟุตเตอร์ -->
+          <center>
+            <h3>ผลงานนักเรียน</h3>
+            <hr />
+            <p>รายวิชา WebApplication</p>
+            <p>โรงเรียนหล่มสักวิทยาคม</p>
+          </center>
+        </div>
+       </div>
+     </div>
+   </div>   
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
